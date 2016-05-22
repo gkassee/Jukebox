5317e8526a7a948b1b63275f2d3937d4df2fb9cf
Greg Kassee <gkassee@gmail.com>
2016-05-22 17:49:18 -0400

    Adding ability to upload a new song and allowing the player to continuously cycle through songs via next and back buttons.

--- JS/Jukebox.js
+++ JS/Jukebox.js
@@ -1,7 +1,8 @@
 
-function Song(Artist, SongTitle, URL) {
+function Song(Artist, SongTitle, AlbumTitle, URL) {
 	this.Artist = Artist;
 	this.SongTitle = SongTitle;
+	this.AlbumTitle = AlbumTitle;
 	this.URL = URL;
 }
 
@@ -31,6 +32,7 @@ var Jukebox = function(){
 		this.play = function(){
 			document.getElementById("SongTitle").innerHTML = this.songs[i].SongTitle;
 			document.getElementById("ArtistName").innerHTML = this.songs[i].Artist;
+			document.getElementById("AlbumTitle").innerHTML = this.songs[i].AlbumTitle;
 			x.play();
 		};
 
@@ -43,12 +45,23 @@ var Jukebox = function(){
 
 		this.next = function() {
 		//Instinct made me want to use a loop for this, but that wasn't correct. 
-			if(i < this.songs.length) {
+			if (i == this.songs.length - 1) {
+				i = 0
+				x.setAttribute("src", this.songs[i].URL)
+				x.currentTime = 0;
+				document.getElementById("SongTitle").innerHTML = this.songs[i].SongTitle;
+				document.getElementById("ArtistName").innerHTML = this.songs[i].Artist;
+				document.getElementById("AlbumTitle").innerHTML = this.songs[i].AlbumTitle;
+				x.play();
+			}
+			else if(i < this.songs.length) {
 				i++ //This means i + 1. Var i will hold the value it's given here
 				x.setAttribute("src", this.songs[i].URL)
 				document.getElementById("SongTitle").innerHTML = this.songs[i].SongTitle;
 				document.getElementById("ArtistName").innerHTML = this.songs[i].Artist;
+				document.getElementById("AlbumTitle").innerHTML = this.songs[i].AlbumTitle;
 				x.play();
+				console.log(i)
 			}
 		}
 
@@ -60,10 +73,17 @@ var Jukebox = function(){
 				x.currentTime = 0;
 				document.getElementById("SongTitle").innerHTML = this.songs[i].SongTitle;
 				document.getElementById("ArtistName").innerHTML = this.songs[i].Artist;
+				document.getElementById("AlbumTitle").innerHTML = this.songs[i].AlbumTitle;
 				x.play();
-			// if (i = 0) {
-			// 	x.play(-1)
-			// }
+			}
+			else if (i == 0) {
+				i = this.songs.length - 1
+				x.setAttribute("src", this.songs[i].URL)
+				x.currentTime = 0;
+				document.getElementById("SongTitle").innerHTML = this.songs[i].SongTitle;
+				document.getElementById("ArtistName").innerHTML = this.songs[i].Artist;
+				document.getElementById("AlbumTitle").innerHTML = this.songs[i].AlbumTitle;
+				x.play();
 			}
 		}
 
@@ -83,10 +103,12 @@ var Jukebox = function(){
 			var a
 			var b
 			var c
+			var d
 			a = document.getElementById("artistName").value
 			b = document.getElementById("songName").value
-			c = document.getElementById("newURL").value
-			var x = new Song(a, b , c)
+			c = document.getElementById("albumName").value
+			d = document.getElementById("newURL").value
+			var x = new Song(a, b , c, d)
 			this.songs.push(x)
 		}
 
@@ -101,23 +123,23 @@ var jukebox = new Jukebox();
 // ----- My Songs ----- \\
 
 
-var PoolsRemix = new Song("Glass Animals", "Pools (Roosevelt Remix)", "./Music/Pools_Roosevelt_Remix.mp3")
-var Suckers = new Song("Ra Ra Riot", "Suckers", "./Music/Suckers.mp3")
-var GuiltTrip = new Song("Kanye West", "Guilt Trip", "./Music/Guilt_Trip.mp3")
-var Controlla = new Song("Drake", "Controlla (feat. Popcaan)", "./Music/Controlla.mp3")
-var GenghisKhan = new Song("Miike Snow", "Genghis Khan", "./Music/Genghis_Khan.mp3")
-var TenLovers = new Song("The Black Keys", "10 Lovers", "./Music/10_Lovers.mp3")
-var MiracleAligner = new Song("The Last Shadow Puppets", "Miracle Aligner", "./Music/Miracle_Aligner.mp3")
+var PoolsRemix = new Song("Glass Animals", "Pools (Roosevelt Remix)", "Glass Animals - Remixes", "./Music/Pools_Roosevelt_Remix.mp3")
+var Suckers = new Song("Ra Ra Riot", "Suckers", "Need Your Light", "./Music/Suckers.mp3")
+var GuiltTrip = new Song("Kanye West", "Guilt Trip", "Yeezus", "./Music/Guilt_Trip.mp3")
+var Controlla = new Song("Drake", "Controlla", "Views", "./Music/Controlla.mp3")
+var GenghisKhan = new Song("Miike Snow", "Genghis Khan", "iii", "./Music/Genghis_Khan.mp3")
+var TenLovers = new Song("The Black Keys", "10 Lovers", "Turn Blue", "./Music/10_Lovers.mp3")
+var MiracleAligner = new Song("The Last Shadow Puppets", "Miracle Aligner", "Everything You've Come To Expect", "./Music/Miracle_Aligner.mp3")
 
 
 
 jukebox.preload(GenghisKhan);
 jukebox.preload(GuiltTrip);
-jukebox.preload(Suckers);
-jukebox.preload(PoolsRemix);
-jukebox.preload(Controlla);
-jukebox.preload(TenLovers);
 jukebox.preload(MiracleAligner);
+jukebox.preload(Suckers);
+jukebox.preload(Controlla);
+jukebox.preload(PoolsRemix);
+jukebox.preload(TenLovers);
 
 jukebox.load()
 
